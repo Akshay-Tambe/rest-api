400163598593b19a275b5502e068d92109bd79e9
card-backend <akshay.tambe@gmoney.in>
2022-01-13 18:31:25 +0530

    pm2

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose');
-const fs = require('fs');
+var http = require('http');
 var cardImageGenerator = require('./routes/cardImageGenerator');
 var mergeImageGenerator = require('./routes/mergeImageGenerator');
 
@@ -44,16 +43,80 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//Set up default mongoose connection
-var mongoDB = 'mongodb+srv://card_db:DDcZMzmuBpQ0Z1x8@cluster0.ws54c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-//Get the default connection
-var db = mongoose.connection;
+/**
+ * Create HTTP server.
+ */
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('connected', () => {
-  console.log('Connected!!')
-});
+var server = http.createServer(app);
 
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log(bind);
+}
 module.exports = app;
